1da0370bef2a3d7e8ff4af86fea488a72a8c0cc0
DarkRezet <darkrezet.music@gmail.com>
2026-07-11 19:36:56 +0300

    Delete src/cogs/test directory

--- src/cogs/test/test.py
+++
@@ -1,7 +0,0 @@
-from telethon import TelegramClient, events
-
-async def setup(client: TelegramClient):
-
-    @client.on(events.NewMessage(pattern='.test'))
-    async def test_cmd(event):
-        await event.edit('Hi')
